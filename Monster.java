2428e90f528b3d5623bc115adb593c1988e7400d
tomk6 <tdk5246@psu.edu>
2025-02-09 20:46:44 -0500

    Classes and Headers

---
+++ Monster.java
@@ -0,0 +1,11 @@
+/** Project: Solo Lab2 Assignment Abstraction, Inheritance, Polymorphism: Monsters
+ * Purpose Details: Practice Abstraction, Inheritance, Polymorphism
+ * Course: IST 242
+ * Author: Thomas Koltes
+ * Date Developed: 2/9/2025
+ * Last Date Changed: 2/9/2025
+ * Rev: 1
+
+ */
+public class Monster {
+}
